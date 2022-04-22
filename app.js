4296518f079098da63ac97f86b38de78e731ef76
Adryan <adryanynfante@gmail.com>
2022-04-22 10:58:34 -0500

    Adryan ok funcines
    funciones listas juego funcional falta css para estilos

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//tenemos 3 objetos 2 barras y la pelota
-
+//necesitamos 3 objetos 2 barras y la pelota
 // funcion para el pizarra
 (function(){
     self.Board = function (width, height){
@@ -30,8 +29,68 @@
         this.ctx = canvas.getContext("2d");
     }
 
+    self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height)
+        },
+        draw: function(){
+            for(var i = this.board.elements.length-1; i>=0; i--){
+                var el = this.board.elements[i];
+
+                draw (this.ctx,el);
+            }
+        },
+        check_collisions: function(){
+            for (let index = 0; index < this.board.bars.length; index++) {
+                var bar = this.board.bars[index];
+                if (hit(bar, this.board.ball)){
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
+        play: function (){
+            if (this.board.playing){
+                this.clean();
+                this.draw();
+                this.check_collisions();
+                this.board.ball.move();   
+            }
+        }
+    }
+    function hit (a,b){
+        //revisa las diferentes colisiones
+        var hit = false;
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= + a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+        }
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
+    }
+    //funciones elementos cuadrado y circulo para barras y pelota
+    function draw (ctx, element){
+        switch (element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x, element.y,element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y, element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
+        }
+    }
 })();
-//barras 
+//funcion para las barras
 (function(){
     self.Bar = function(x,y,width,height,board){
         this.x = x;
@@ -44,7 +103,7 @@
         this.speed= 20;
 
     }
-    //movimineto barras
+    //movimientos
     self.Bar.prototype ={
         down: function(){
             this.y += this.speed;
@@ -55,24 +114,51 @@
         },
     }
 })();
-//funciones elementos cuadrado y circulo para barras y pelota
-function draw (ctx, element){
-    switch (element.kind){
-        case "rectangle":
-            ctx.fillRect(element.x, element.y,element.width, element.height);
-            break;
-        case "circle":
-            ctx.beginPath();
-            ctx.arc(element.x,element.y, element.radius,0,7);
-            ctx.fill();
-            ctx.closePath();
-            break;
+//funcion pra pelota  y movimientos
+(function(){
+    self.Ball = function(x,y,radius,board){
+        console.log("constructor ball")
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+        this.bounceAngle = 0;
+        this.maxBounceAngle = Math.PI / 12;
+        this.speed = 3;
+        board.ball = this;
+        this.kind = "circle";
     }
-}
+    self.Ball.prototype = {
+        move: function (){
+            this.x += (this.speed_x*this.direction);
+            this.y += (this.speed_y*this.direction);
+        },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+        collision: function (bar){
+            var relativeIntersectY = (bar.y + (bar.height / 2)) - this.y;
+            var normalizedIntersectY = relativeIntersectY / (bar.height / 2);
+            this.bounceAngle = normalizedIntersectY * this.maxBounceAngle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounceAngle);
+            this.speed_x = this.speed * Math.cos(this.bounceAngle);
+
+            if (this.x > (this.board.width / 2)) {
+                this.direction = -1;
+            } else {
+                this.direction = 1;
+            }
+        }
+    }
 })();
 
-
-//pintar pizarra en index canvas...
 var board = new Board(800,400);
 console.log(board);
 var bar = new Bar (20,150,10,100,board)
@@ -80,3 +166,32 @@ var bar2 = new Bar (770,150,10,100,board)
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
 var ball = new Ball(350,200,7,board);
+
+//eventos teclas
+document.addEventListener("keydown", function (ev){
+    if(ev.keyCode == 38){
+        ev.preventDefault();
+        bar2.up();
+    }else if (ev.keyCode == 40){
+        ev.preventDefault();
+        bar2.down();
+    }else if(ev.keyCode == 87){
+        ev.preventDefault();
+        bar.up();
+    }else if (ev.keyCode == 83){
+        ev.preventDefault();
+        bar.down();
+    }else if (ev.keyCode == 32){
+        ev.preventDefault();
+        board.playing= !board.playing;
+    } 
+});
+
+board_view.draw();
+
+window.requestAnimationFrame(controller);
+
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
+}
